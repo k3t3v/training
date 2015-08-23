b36c4a806557ec213b91d97b80d3946d6c284212
M Pavel <k3t3v@users.noreply.github.com>
2015-08-23 23:39:52 +1000

    update php script

--- hello.php
+++ hello.php
@@ -3,6 +3,21 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+  <p>
+ <?php
+  // Declare the variable 'string' and assign it a value.
+  // The <br> is the HTML equivalent to a new line.
+  $string = 'Hello World!<br>';
+
+  // You can echo the variable, similar to the way you would echo a string.
+  echo $string;
+
+  // You could also use print.
+  print $string;
+
+  // Or, if you are familiar with C, printf can be used too.
+  printf('%s', $string);
+?>
+  </p>
  </body>
 </html>
